6708fc941a8d4fa3196037f2d11202141f3eb074
MortenBE <Ehlersmorten@gmail.com>
2020-05-18 01:25:23 +0200

    asd

--- v3/modelsmanagementfrontend2.0/src/views/Model.vue
+++ v3/modelsmanagementfrontend2.0/src/views/Model.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="manage">
         <div class="card">
             <JobList />
         </div>
@@ -17,19 +17,26 @@
     }
 </script>
 
-<style scoped>    
+<style scoped>  
+        .manage
+    {        
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
     .card {
         box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
         margin: 10px;
         transition: 0.3s;
         width: 500px;
         border-radius: 5px;
-        text-align:center;
-        background-color:white;
-    } 
-    .card:hover {
-        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+        text-align: center;
+        background-color: white;
     }
+
+        .card:hover {
+            box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+        }
 </style>
 
 
